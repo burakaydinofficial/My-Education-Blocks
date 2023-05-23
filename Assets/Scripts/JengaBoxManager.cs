5e4a4e8c4cedce44ef26ced3024587e2148d66b3
Burak AYDIN <burakk.mobile@gmail.com>
2023-05-23 19:46:23 +0300

    Turntable for JengaBoxManager

--- Assets/Scripts/JengaBoxManager.cs
+++ Assets/Scripts/JengaBoxManager.cs
@@ -1,27 +1,35 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class JengaBoxManager : MonoBehaviour
 {
     [SerializeField] private Transform _root;
+    [SerializeField] private Vector3 _baseOffset = new Vector3(0f, 0.75f, 0f);
     [SerializeField] private Vector3 _layerOffset = new Vector3(0f, 1.5f, 0f);
     [SerializeField] private Vector3 _rowOffset = new Vector3(0f, 0f, 2.5f);
     [SerializeField] private GameObject _jengaBoxPrefab;
-    [SerializeField] private string _gradeTag = "6th Grade";
+    [SerializeField, FormerlySerializedAs("_gradeTag")] private string _defaultGradeTag = "6th Grade";
+    [SerializeField] private TextMeshProUGUI _tagText;
 
+    [NonSerialized] private string m_gradeTag;
     [NonSerialized] private List<JengaBoxController> _instances = new List<JengaBoxController>();
 
-    public void Set(StackApiRequest.StackApiDataElement[] data, Action<JengaBoxController> clickCallback)
+    public void Set(StackApiRequest.StackApiDataElement[] data, Action<JengaBoxController> clickCallback, string gradeTag = null)
     {
         var instanceIndex = 0;
 
         var rotatedRowOffset = new Vector3(_rowOffset.z, _rowOffset.y, _rowOffset.x);
 
+        m_gradeTag = gradeTag ?? _defaultGradeTag;
+        Debug.Log("Initializing " + m_gradeTag);
+
         for (var i = 0; i < data.Length; i++)
         {
             var element = data[i];
-            if (!string.Equals(element.grade, _gradeTag))
+            if (!string.Equals(element.grade, m_gradeTag))
                 continue;
 
             if (instanceIndex >= _instances.Count)
@@ -30,7 +38,7 @@ public class JengaBoxManager : MonoBehaviour
                 var layer = instanceIndex / 3;
                 var inLayer = instanceIndex % 3;
                 var rowOffsetForLayer = (layer % 2 == 0 ? _rowOffset : rotatedRowOffset);
-                var position = _layerOffset * layer + (inLayer - 1) * rowOffsetForLayer;
+                var position = _baseOffset + _layerOffset * layer + (inLayer - 1) * rowOffsetForLayer;
                 newInstance.transform.localPosition = position;
                 newInstance.transform.localEulerAngles = new Vector3(0f, (layer % 2) * 90f, 0f);
                 _instances.Add(newInstance.GetComponent<JengaBoxController>());
@@ -39,6 +47,9 @@ public class JengaBoxManager : MonoBehaviour
             _instances[instanceIndex].Set(element, clickCallback);
             instanceIndex++;
         }
+
+        if (_tagText)
+            _tagText.text = m_gradeTag;
     }
 
     public void Hide()
--- Assets/Scripts/TestMyStackController.cs
+++ Assets/Scripts/TestMyStackController.cs
@@ -1,6 +1,86 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+
+public class TurnTable
+{
+    private readonly Transform _root;
+    public readonly float AngleRange;
+    private readonly float _radius;
+    private readonly int _count;
+    private readonly List<TurnTableElement> _elements = new List<TurnTableElement>();
+
+    public TurnTable(Transform root, float angleRange, float radius, int count)
+    {
+        _root = root;
+        AngleRange = angleRange;
+        _radius = radius;
+        _count = count;
+    }
+
+    public TurnTableElement Deploy(Transform t, int index)
+    {
+        t.SetParent(_root, false);
+        var e = new TurnTableElement(this, t, index);
+        _elements.Add(e);
+        return e;
+    }
+
+    public TurnTableElement Deploy(Transform t)
+    {
+        return Deploy(t, _elements.Count);
+    }
+
+    public void Select(int index)
+    {
+        Position(-CalculateAngle(index));
+    }
+
+    public void Position(float angle)
+    {
+        foreach (var turnTableElement in _elements)
+        {
+            turnTableElement.Position(angle + (AngleRange * 0.5f));
+        }
+
+        _root.transform.localPosition = SamplePoint(AngleRange * 0.5f, _radius);
+    }
+
+    private float CalculateAngle(int index)
+    {
+        return Mathf.Lerp(0f, AngleRange, index / (_count - 1f));
+    }
+
+    public static Vector3 SamplePoint(float angle, float radius)
+    {
+        return new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle) * radius, 0f,
+            Mathf.Sin(Mathf.Deg2Rad * angle) * radius);
+    }
+
+    public class TurnTableElement
+    {
+        private readonly TurnTable _btt;
+        private readonly Transform _t;
+        private readonly int _index;
+
+        public TurnTableElement(TurnTable btt, Transform t, int index)
+        {
+            _btt = btt;
+            _t = t;
+            _index = index;
+            Position(0f);
+        }
+
+        public void Position(float mainAngle)
+        {
+            float angle = _btt.CalculateAngle(_index) + mainAngle;
+            _t.localPosition = SamplePoint(angle, _btt._radius);
+            _t.localRotation = Quaternion.AngleAxis(-90f - angle, Vector3.up);
+        }
+    }
+}
 
 public class TestMyStackController : MonoBehaviour
 {
@@ -12,9 +92,10 @@ public class TestMyStackController : MonoBehaviour
     [SerializeField] private GameObject _loadingScreenRoot;
     [SerializeField] private GameObject _errorScreenRoot;
 
-    [SerializeField] private JengaBoxManager _6ThGradeJengaBoxManager;
-    [SerializeField] private JengaBoxManager _7ThGradeJengaBoxManager;
-    [SerializeField] private JengaBoxManager _8ThGradeJengaBoxManager;
+    [SerializeField] private Transform _stageRoot;
+    [SerializeField] private GameObject _stagePrefab;
+
+    [NonSerialized] private Dictionary<string, JengaBoxManager> _boxManagers = new Dictionary<string, JengaBoxManager>();
 
     [SerializeField] BoxInfoController _boxInfoController;
     [SerializeField] private LayerMask _clickLayerMask;
@@ -23,6 +104,11 @@ public class TestMyStackController : MonoBehaviour
 
     [NonSerialized] private JengaBoxController _selected;
     [NonSerialized] private JengaBoxController _highlighted;
+
+    [NonSerialized] private TurnTable _turnTable;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float _turnTablePosition = 0f;
 
     private IEnumerator Start()
     {
@@ -61,9 +147,25 @@ public class TestMyStackController : MonoBehaviour
         _data = stackApiRequest.Data;
         Array.Sort(_data, Sorter);
 
-        _6ThGradeJengaBoxManager.Set(_data, ClickCallback);
-        _7ThGradeJengaBoxManager.Set(_data, ClickCallback);
-        _8ThGradeJengaBoxManager.Set(_data, ClickCallback);
+        HashSet<string> grades = new HashSet<string>();
+        foreach (var stackApiDataElement in _data)
+        {
+            grades.Add(stackApiDataElement.grade);
+        }
+
+        _turnTable = new TurnTable(_stageRoot, 180f, grades.Count * 30f, grades.Count);
+        
+        foreach (var grade in grades)
+        {
+            var go = Instantiate(_stagePrefab, _stageRoot, false);
+
+            _turnTable.Deploy(go.transform);
+
+            var manager = go.GetComponent<JengaBoxManager>();
+            _boxManagers[grade] = manager;
+            manager.Set(_data, ClickCallback, grade);
+        }
+
         SetLoadingScreen(false);
 
         _started = true;
@@ -84,6 +186,9 @@ public class TestMyStackController : MonoBehaviour
     {
         if (_started && !_starting)
             CheckClicks();
+
+        if (_turnTable != null)
+            _turnTable.Position(Mathf.Lerp(0f, _turnTable.AngleRange, _turnTablePosition));
     }
 
     private int _clickDownFrame = -1;
